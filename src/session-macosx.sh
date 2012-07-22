284e9a3026434b37c9a96de440374b11b33ca511
Rubin Simons <rubin@raaftech.com>
2012-07-22 10:16:57 +0000

    Posthumously added build.xml and release.xml to enable automated building of this release and added updated version command.

--- src/session-macosx.sh
+++ src/session-macosx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version 0.7.9, released 24-01-2011.
+# RELEASE.STRING.VERSION
 #
 # There are known speed issues with this script on Vista and 7.
 # These issues are related to cygwin's fork() performance which
@@ -2126,7 +2126,11 @@ case "$1" in
     esac
     ;;
     version)
-    cat $0 | grep "^# Version" | sed "s|^# ||g"
+    echo "RELEASE.STRING.VERSION
+    RELEASE.STRING.COPYRIGHT
+    RELEASE.STRING.RELDATE
+    RELEASE.STRING.BUILT
+    RELEASE.STRING.LICENSE" | sed 's/^[ \t]*//;s/[ \t]*$//'
     ;;
     *)
     echo $"Usage: $0 command {group, guest, host or special argument}"
