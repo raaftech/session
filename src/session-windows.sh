5084b427caf5349a5d9bb20b69f77646ffcc2658
Rubin Simons <rubin@raaftech.com>
2012-07-22 09:52:44 +0000

    Posthumously added build.xml and release.xml to enable automated building of this release and added updated version command.

--- src/session-windows.sh
+++ src/session-windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Version 0.7.1, released 31-12-2010.
+# RELEASE.STRING.VERSION
 #
 # There are known speed issues with this script on Vista and 7.
 # These issues are related to cygwin's fork() performance which
@@ -1913,7 +1913,11 @@ case "$1" in
     esac
     ;;
     version)
-    cat $0 | grep "^# Version" | sed "s|^# ||g"
+    echo "RELEASE.STRING.VERSION
+    RELEASE.STRING.COPYRIGHT
+    RELEASE.STRING.RELDATE
+    RELEASE.STRING.BUILT
+    RELEASE.STRING.LICENSE" | sed 's/^[ \t]*//;s/[ \t]*$//'
     ;;
     *)
     echo $"Usage: $0 command {group, guest, host or special argument}"
