5bc3f172f1942298481adca20a61316c0b6cdf4e
Thomas Hood <thomas@raaftech.com>
2012-09-28 09:19:56 +0000

    Trivial change

--- functions.sh
+++ functions.sh
@@ -3863,7 +3863,7 @@ function smbExecHandler {
         ;;
     esac
 
-    if [ "$platform" = "linux" ]; then 
+    if [ "$platform" = "linux" ]; then
         # If gnome-keyring-daemon is running, check to see if a prompt has started, wait for it to close.
         waitForDialog "gnome-keyring-daemon" "gnome-keyring-prompt"
     elif [ "$platform" = "macosx" ]; then
