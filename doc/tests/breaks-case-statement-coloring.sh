79a1d8f23c32640e8d86e7833c5dda297a33bcaf
Rubin Simons <rubin@raaftech.com>
2013-07-31 13:29:27 +0000

    Added little test which breaks syntax highlighting for Sublime Text 2.

---
+++ doc/tests/breaks-case-statement-coloring.sh
@@ -0,0 +1,135 @@
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
+
+case "$meh" in
+    foo)
+    echo "foo: $foo"
+    bar)
+    echo "bar: $bar"
+    *)
+    echo "eek!"
+esac
