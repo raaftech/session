8083019c577c5a92ba59de143c21e5adefc5c5fb
Rubin Simons <rubin@raaftech.com>
2016-04-08 11:31:30 +0200

    Added sound redirection support to xfreerdp handler.

--- functions.sh
+++ functions.sh
@@ -3864,7 +3864,7 @@ function xfreerdpDesktopHandler {
     rdpFileWriter
     if [ "$xsasupwd" ]; then typeset pass="/p:$xsasupwd"; fi
     if [ "$titling" ]; then typeset title="/t:$(capsFirst "$name")"; fi
-    xfreerdp /v:$addr /u:"$xsasuser" $pass $title /f -bitmap-cache -offscreen-cache -glyph-cache /cert-ignore +auto-reconnect +clipboard +fonts +aero +window-drag +wallpaper +themes > /dev/null 2>&1 &
+    xfreerdp /v:$addr /u:"$xsasuser" $pass $title /f /sound:sys:alsa -bitmap-cache -offscreen-cache -glyph-cache /cert-ignore +auto-reconnect +clipboard +fonts +aero +window-drag +wallpaper +themes > /dev/null 2>&1 &
 }
 
 # noneAccessHandler(state|access)
